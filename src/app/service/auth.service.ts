d857bfcd4c6b1d1284aed32d82045df7606b72d4
n00bpr0gram3r <abdulb4s1tbakz@gmail.com>
2023-11-01 14:14:59 +0530

    V_2.0

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -5,7 +5,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-  baseAPI = 'https://campus-connect-json-server.onrender.com/users'
+  baseAPI = 'https://campusconnect-json-server.onrender.com/users'
+  // baseAPI = 'http://localhost:3000/users'
   constructor(private http: HttpClient) { }
 
   //All User 
@@ -22,17 +23,17 @@ export class AuthService {
     return this.http.post(this.baseAPI, inputdata)
   }
   //Update User Data
-  updateData(id:any, inputdata:any){
-    return this.http.put(this.baseAPI+'/'+id,inputdata)
+  updateData(id: any, inputdata: any) {
+    return this.http.put(this.baseAPI + '/' + id, inputdata)
   }
 
   //Get username to validate login
-  isLoggedIn(){
-    return sessionStorage.getItem('username')!=null
+  isLoggedIn() {
+    return sessionStorage.getItem('username') != null
   }
 
   //Get role to validate the permissons
-  getUserRole(){
-    return sessionStorage.getItem('role')!=null?sessionStorage.getItem('role')?.toString():''
+  getUserRole() {
+    return sessionStorage.getItem('role') != null ? sessionStorage.getItem('role')?.toString() : ''
   }
 }
